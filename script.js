ca00010a8da0779e7e4b81c15417a7af6cb7a42a
AAKASH PANDITA <aakash2711522@gmail.com>
2020-09-01 10:46:53 +0530

    updating script
    updating script

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById("btn").onclick = function() {
 var xhttp = new XMLHttpRequest();
 var city_name = document.getElementById("input").value;
 var api_key = "1e6af43b1699f8cbaa03a3bda0ae5290";
-var url = "http://api.openweathermap.org/data/2.5/weather?q=" + city_name 
+var url = "https://api.openweathermap.org/data/2.5/weather?q=" + city_name 
 + "&units=metric&appid=" + api_key;
 xhttp.onreadystatechange = function () {
     if (xhttp.readyState == 4 && xhttp.status == 200) {
